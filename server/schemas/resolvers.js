06c5f8eea14c198106826b21a34bde62b9b21de9
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-07 17:39:38 -0500

    Bring over getSingleUser function from user-controller.js into resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,19 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-module.exports ={}
+module.exports ={
+    Query: {
+        async getSingleUser({ user = null, params }, res) {
+            const foundUser = await User.findOne({
+                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+            });
+        
+            if (!foundUser) {
+                return res.status(400).json({ message: 'Cannot find a user with this id!' });
+            }
+        
+            res.json(foundUser);
+            },
+    },
+    
+}
