9228e22f175b0e131c9f9273189c28fd868a34b3
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-08 14:44:51 -0500

    Add type User & type Query to typeDefs.js

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,1 +1,13 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        username : String
+        email: String
+        password: String
+        SavedBooks: [Book]
+    }
+    type Query {
+        me: User
+    }
+`
