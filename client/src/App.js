f4855984bb8d57736b547badea245cbbac928198
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-12 14:34:49 -0500

    Import setContext and create variables for createHttpLink and authLink

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,22 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
