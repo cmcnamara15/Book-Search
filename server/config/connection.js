dd4133c6b5c4304c3797acfc3054f40ec2e102dd
Chris McNamara <cmcnamara1991@icloud.com>
2023-07-14 22:40:16 -0500

    Revert and uncomment code in config.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,5 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-// module.exports = mongoose.connection;
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-database-name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
